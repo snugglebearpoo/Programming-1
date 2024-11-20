5e6fe259d8280fe8d276edc97f8832db980b438a
snugglebearpoo <Mark.good38@gmail.com>
2024-11-19 21:23:43 -0500

    Add files via upload

---
+++ PersonInfo.java
@@ -0,0 +1,21 @@
+public class PersonInfo {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String firstName = "John";
+        String lastName = "Doe";
+        String streetAddress = "123 Main Street";
+        String city = "Any town";
+        String zipCode = "12345";
+
+        System.out.println(firstName);
+        System.out.println(lastName);
+        System.out.println(streetAddress);
+        System.out.println(city);
+
+        System.out.println(zipCode);
+
+
+    }
+}
